a9352984963eaf2bd0368ef127515f9fe2f86a5b
n.titov <n.titov@bftcom.com>
2022-08-13 17:29:39 +0300

    Научился вводить сообщение из терминла

--- app.go
+++ app.go
@@ -3,5 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hellow Orld")
+	var Name, Age = "Bear", 12
+	var c = fmt.Sprintf(Name, Age)
+	fmt.Println("Hellow Orld#3")
+	fmt.Println(c)
+	fmt.Println("Print your name plz:")
+	fmt.Scanln(&Name)
+	fmt.Println("You entered:" + Name)
 }
