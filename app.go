a45985542632ea4aa9171402e8a7993bd9270c0e
n.titov <n.titov@bftcom.com>
2022-08-13 18:41:10 +0300

    Комит из голандии

--- app.go
+++ app.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 func main() {
+	var Ali string = "Here"
 	var Name, Age = "Bear", 12
 	var c = fmt.Sprintf(Name, Age)
 	fmt.Println("Hellow Orld#3")
@@ -10,4 +11,9 @@ func main() {
 	fmt.Println("Print your name plz:")
 	fmt.Scanln(&Name)
 	fmt.Println("You entered:" + Name)
+
+	var Foo int = 123
+	var result string
+	result = fmt.Sprintf("Ёбаные якоря %d %s", Foo, Ali)
+	fmt.Println(result)
 }
