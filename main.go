6a39e4c34d8989fe4e7fc169c11df282ff1caf63
Nikolai Titov <mudor.mt.dm@outlook.com>
2022-08-14 13:11:28 +0300

    Testing go funcktions in main.

--- main.go
+++ main.go
@@ -1,17 +1,5 @@
 package main
 
-import (
-	"log"
-	"net/http"
-)
-
-func Handler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("<b>Hellow<b> <i>Orld!<i>\n"))
-}
-
-// Комментарий для проверки коммита
 func main() {
-	http.HandleFunc("/", Handler)
-	log.Println("Start http server on port 8081")
-	log.Fatal(http.ListenAndServe(":8081", nil))
+
 }
