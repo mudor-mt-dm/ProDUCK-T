6fcc27a9f64d364fc325bf1f57e750db9026049e
Nikolai Titov <mudor.mt.dm@outlook.com>
2022-08-14 18:55:33 +0300

    folded functions

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import "fmt"
 
+func totalPrice (InitProce int) func(int) int{
+	sum :=InitProce
+	return func(x int) int {
+		sum+=x
+		return sum
+	}
 }
+
+func main(){
+	orderPrice:= totalPrice(1)
+	fmt.Println(orderPrice(3))
+	fmt.Println(orderPrice(6))
+}
+
