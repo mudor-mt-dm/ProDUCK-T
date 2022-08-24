54b467b66a83a98f75da6ce04ea9157c0944a7fc
Nikolai Titov <mudor.mt.dm@outlook.com>
2022-08-24 13:18:30 +0300

    Some changes

--- cmd/arrays/main.go
+++ cmd/arrays/main.go
@@ -1,3 +1,43 @@
 package main
 
+import (
+	"fmt"
+	"math/bits"
+)
+
+type Point struct {
+	X int
+	Y int
+}
+
+func main() {
+	Newmap := map[string]Point{}
+	// Newmap is a mapped by strings array with points
+	Newmap
+	fmt.Println(Newmap)
+
+	array := []int{1, 2, 3}
+	var max int = (1 << bits.UintSize) / -2
+	var min int = (1 << bits.UintSize) / 2 - 1
+	for _, el := range array {
+		if el > max {max = el}
+		if el < min {max = el}
+		}
+	}
+}
+
 // Здесь потестируем всякие массивы
+/*
+slice := []string{"a", "b", "c"}
+//перебрали массив и элементы
+for index, element := range slice {
+	fmt.Printf("element #%d: %s\n", index, element)
+}
+// перебрали элементы
+for _, element := range slice {
+	fmt.Printf("element without number: %s\n", element)
+}
+//перебрали индексы
+for index := range slice {
+	fmt.Printf("element without number: %s\n", index)
+}*/
